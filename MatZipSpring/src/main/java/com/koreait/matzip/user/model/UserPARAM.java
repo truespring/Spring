18aa25468602e17223cc37f7be4a18196e79c62c
truespring <true_spring_@naver.com>
2020-09-21 10:43:43 +0900

    로그인 완료

---
+++ MatZipSpring/src/main/java/com/koreait/matzip/user/model/UserPARAM.java
@@ -0,0 +1,13 @@
+package com.koreait.matzip.user.model;
+
+public class UserPARAM extends UserVO {
+	private String msg;
+
+	public String getMsg() {
+		return msg;
+	}
+
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+}
